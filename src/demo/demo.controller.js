b7c7b524d4ae8902b96d3f0c877b3357224a625e
Dexter Juntilla <dexter@vananaz.com>
2019-03-06 13:57:08 +0800

    partial pagination request

--- src/demo/demo.controller.js
+++ src/demo/demo.controller.js
@@ -12,80 +12,54 @@ export const createCar = (admin: Object) => (req: Object, res: Object) => {
   }
 };
 
-export const fetchCars = (req: Object, res: Object, next: Function) => {
-  let final = [...cars];
-  const { query } = req;
+export const fetchCars = (admin: Object) => (req: Object, res: Object) => {
+  try {
+    const db = admin.firestore();
+    const { query } = req;
 
-  if (query) {
-    const filterKeys = Object.keys(query)
-      .filter(key => key.toLowerCase().indexOf('_like') > -1)
-      .map(key => key.substr(0, key.length - '_like'.length));
+    let carsQuery = db.collection('cars');
 
-    if (query.q) {
-      final = final.filter(obj => {
-        const boolArray = Object.keys(obj).map(key => {
-          return (
-            obj[key]
-              .toString()
-              .toLowerCase()
-              .indexOf(query.q.toLowerCase()) > -1
-          );
+    if (query) {
+      const filterKeys = Object.keys(query)
+        .filter(key => key.toLowerCase().indexOf('_like') > -1)
+        .map(key => key.substr(0, key.length - '_like'.length));
+
+      if (filterKeys.length > 0) {
+        filterKeys.forEach(field => {
+          const key = `${field}_like`;
+          carsQuery = carsQuery.where(field, '>=', query[key]);
         });
-
-        return boolArray.find(el => el === true);
-      });
-    }
-
-    if (filterKeys.length > 0) {
-      final = final.filter(obj => {
-        const boolArray = filterKeys.map(key => {
-          return (
-            obj[key]
-              .toString()
-              .toLowerCase()
-              .indexOf(query[`${key}_like`].toLowerCase()) > -1
-          );
-        });
-
-        return typeof boolArray.find(el => el === false) === 'undefined';
-      });
-    }
-
-    if (query._sort) {
-      final = final.sort((el1, el2) => {
-        const condition =
-          typeof el1[query._sort] === 'string'
-            ? el1[query._sort].toLowerCase() < el2[query._sort].toLowerCase()
-            : el1[query._sort] < el2[query._sort];
-        if (condition) {
-          if (query._order && query._order === 'desc') {
-            return 1;
-          }
-
-          return -1;
-        }
-        if (el1[query._sort] > el2[query._sort]) {
-          if (query._order && query._order === 'desc') {
-            return -1;
-          }
-
-          return 1;
-        }
-
-        return 0;
-      });
-    }
-
-    if (query._page) {
-      let limit = 5;
-
-      if (query._limit) {
-        limit = parseInt(query._limit, 10);
       }
-      final = final.slice(query._page * limit, query._page * limit + limit);
-    }
-  }
 
-  res.json({ totalRecords: cars.length, data: final });
-  next();
+      if (query._sort) {
+        if (query._order && query._order === 'desc') {
+          carsQuery = carsQuery.orderBy(query._sort, 'desc');
+        } else {
+          carsQuery = carsQuery.orderBy(query._sort);
+        }
+      }
+
+      // if (query._page) {
+      //   let limit = 5;
+
+      //   if (query._limit) {
+      //     limit = parseInt(query._limit);
+      //   }
+
+      //   //   final = final.slice(query._page * limit, query._page * limit + limit);
+      // }
+    }
+
+    carsQuery.get().then(snapshot => {
+      const cars = [];
+
+      snapshot.forEach(doc => {
+        cars.push(doc.data());
+      });
+
+      res.json({ totalRecords: 100, data: cars });
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
