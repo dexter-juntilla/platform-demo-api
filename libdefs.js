53683965191b93f0ce96145308a798ac123d4921
Dexter Juntilla <dexter@vananaz.com>
2019-03-06 13:42:22 +0800

    install modules

--- libdefs.js
+++ libdefs.js
@@ -7,3 +7,27 @@ declare module 'firebase-functions' {
 declare module 'express' {
   declare module.exports: any;
 }
+declare module 'firebase' {
+  declare module.exports: any;
+}
+declare module 'firebase-admin' {
+  declare module.exports: any;
+}
+declare module 'firebase/app' {
+  declare module.exports: any;
+}
+declare module 'firebase/auth' {
+  declare module.exports: any;
+}
+declare module 'firebase/database' {
+  declare module.exports: any;
+}
+declare module 'body-parser' {
+  declare module.exports: any;
+}
+declare module 'cookie-parser' {
+  declare module.exports: any;
+}
+declare module '@babel/polyfill' {
+  declare module.exports: any;
+}
