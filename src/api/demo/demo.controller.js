8a39a8365d2a47f724df73fa694478b8ee690efb
Dexter Juntilla <dexter@vananaz.com>
2019-03-12 17:08:22 +0800

    aggregation count cars

--- src/api/demo/demo.controller.js
+++ src/api/demo/demo.controller.js
@@ -1,11 +1,29 @@
 import { buildFireStoreQuery } from '../../util/common';
 
-export const createCar = (admin: Object) => (req: Object, res: Object) => {
+export const createCar = (admin: Object) => async (
+  req: Object,
+  res: Object,
+) => {
   try {
     const db = admin.firestore();
     const { brand, color } = req.body;
 
-    db.collection('cars').add({ brand, color });
+    const carsCountRef = db.collection('aggregation').doc('cars');
+
+    const carRef = db.collection('cars').doc();
+
+    await db.runTransaction(transaction =>
+      transaction.get(carsCountRef).then(carsCountDoc => {
+        if (!carsCountDoc.exists) {
+          throw Error('Document does not exist!');
+        }
+
+        const newCount = carsCountDoc.data().count + 1;
+
+        transaction.update(carsCountRef, { count: newCount });
+        transaction.set(carRef, { brand, color });
+      }),
+    );
 
     res.status(200).json({ message: 'ok' });
   } catch (err) {
@@ -13,29 +31,34 @@ export const createCar = (admin: Object) => (req: Object, res: Object) => {
   }
 };
 
-export const fetchCars = (admin: Object) => (req: Object, res: Object) => {
+export const fetchCars = (admin: Object) => async (
+  req: Object,
+  res: Object,
+) => {
   try {
     const db = admin.firestore();
     const { query } = req;
+
+    const carsCount = await db
+      .collection('aggregation')
+      .doc('cars')
+      .get();
+
+    const { count } = carsCount.data();
 
     let carsQuery = db.collection('cars');
 
     carsQuery = buildFireStoreQuery(query, carsQuery);
 
-    carsQuery
-      .get()
-      .then(snapshot => {
-        const cars = [];
+    const carsResult = await carsQuery.get();
 
-        snapshot.forEach(doc => {
-          cars.push(doc.data());
-        });
+    const cars = [];
 
-        res.json({ totalRecords: 100, data: cars });
-      })
-      .catch(err => {
-        res.status(500).send(err);
-      });
+    carsResult.forEach(doc => {
+      cars.push(doc.data());
+    });
+
+    res.json({ totalRecords: count, data: cars });
   } catch (err) {
     res.status(500).send(err);
   }
